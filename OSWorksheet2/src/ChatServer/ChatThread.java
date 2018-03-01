75424936e6d6326d31ff0daa4aed857cc9476821
lxf736 <lxf736@cs.bham.ac.uk>
2018-03-01 14:49:56 +0000

    OSWorksheet2 - sign in and sign up working

--- OSWorksheet2/src/ChatServer/ChatThread.java
+++ OSWorksheet2/src/ChatServer/ChatThread.java
@@ -9,48 +9,117 @@ import Protocol.SimpleProtocol;
 
 public class ChatThread extends Thread {
 	
+	private Server server;
     private Socket socketForClient; 
-    private BufferedReader input;
-    private DataOutputStream output;
+    private BufferedReader fromClient;
+    private DataOutputStream toClient;
     private SimpleProtocol protocol;
+    private String user;
     private boolean stopped; 
 	
-	public ChatThread (Socket socketForClient, BufferedReader threadInput, DataOutputStream threadOutput) {
+	public ChatThread (Server server, Socket socketForClient, BufferedReader fromClient, DataOutputStream toClient)  {
+		
+		this.server = server;
 		this.socketForClient = socketForClient;
-		this.input = threadInput;
-		this.output = threadOutput;
+		this.fromClient = fromClient;
+		this.toClient = toClient;
+		this.protocol = new SimpleProtocol();
+		
+		try {
+			toClient.writeBytes(protocol.createMessage("welcome", "Welcome to my server") + "\n");
+		}  catch (IOException e) {
+			System.out.println("client failed to establish connection with server at socket " + socketForClient);
+			e.printStackTrace();
+		}
 	}
+    
+    private void signUp(String user, String pw) throws IOException {
+		if (server.checkUser(user)) {
+			toClient.writeBytes(protocol.createMessage("sign-up", "false", "user already exists, please try signing in") + "\n");
+		} else if (user.length() < 5 || user.length() > 20) {
+			toClient.writeBytes(protocol.createMessage("sign-up", "false", "username must be between 5 and 20 characters in length") + "\n");
+		} else if (pw.length() < 8 || pw.length() >32) {
+			toClient.writeBytes(protocol.createMessage("sign-up", "false", "password must be between 8 and 32 characters in length") + "\n");
+		} else {
+			server.addUser(user, pw);
+			toClient.writeBytes(protocol.createMessage("sign-up", "true", "sign up successful, please sign in with these credentials") + "\n");
+			stopThread();
+		}
+}
+
+	private void signIn(String user, String pw) throws IOException {
+	    this.user = user;
+	    if (server.checkUser(user)) {
+	        toClient.writeBytes(protocol.createMessage("sign-in", "true", "welcome back, user") + "\n");
+	    } else {
+	    		toClient.writeBytes(protocol.createMessage("sign-in", "false", "password/username does not match") + "\n");
+	    }
+	}
+    
 	
 	public void run(){
-    		try {
-    			output.writeBytes("Welcome to my server");
-    		} catch (IOException e) {
-    			System.out.println("client failed to establish connection with server at socket " + socketForClient);
-    			e.printStackTrace();
-    		}
-        while (!stopped) {
-        		processMessage();
-        }
-    }
+		while(true)
+			try {
+				if (!stopped) {
+		    			String currentMessage[] = interpretMessage();
+		    			if (currentMessage.length > 0) {
+		    				processMessage(currentMessage);
+		    			}
+				} else {
+					break;
+				}
+	    } catch (IllegalArgumentException | IOException e) {
+	    		//TO DO
+	    }
+	}
 	
-    public void processMessage() {
-        String line = input.readLine();
-        if (line != null) {
-        		String [] message = protocol.decodeMessage(line);
-        		if (message[0] == "sign-in") {
-        			signIn();
-        		} else if (message[0] == "sign-up") {
-        			signUp();
-        		} else if (message[0] == "get-message") {
+	public String [] interpretMessage() throws IOException {
+		String clientCommand = fromClient.readLine();
+        if (clientCommand != null) {
+        		System.out.println(protocol.decodeMessage(clientCommand));
+            return protocol.decodeMessage(clientCommand);
+        } else {
+        		return new String [0];
+        }
+		
+	}
+	
+    public void processMessage(String [] message) throws IllegalArgumentException, IOException {
+        if (message.length != 0) {
+        		if (message[0].equals("sign-in")) {
+        			signIn(message[1], message[2]);
+        		} else if (message[0].equals("sign-up")) {
+        			signUp(message[1], message[2]);
+        		} //TO DO - SECTION TO HANDLE ACTUAL MESSAGES!!
+        		/*else if (message[0].equals("get-message")) {
         			getMessage();
-        		} else if (message[0] == "send-message") {
+        		} else if (message[0].equals("send-message")) {
         			sendMessage();
-        		} else {
-        			throw new IllegalArgumentException("Message identifier not recognised");
+        		} */else {
+        			throw new IllegalArgumentException("Command identifier not recognised");
         		}
         }
     }
+    
+   
+    
+    private void stopThread() {
+        this.stopped = true;
+        try {
+			socketForClient.close();
+			System.out.println(socketForClient + " connection closed");
+		} catch (IOException e) {
+			System.out.println( socketForClient + " socket not Closed!");
+		}
+        this.interrupt();
+        System.out.println(this.getName() + " terminated");
+    }
+    
+    
+
 	
 	
 
 }
+
+
--- OSWorksheet2/src/ChatServer/Server.java
+++ OSWorksheet2/src/ChatServer/Server.java
@@ -7,51 +7,65 @@ import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 
 public class Server {
 	
-	public static void main (String [] args) throws IOException {
-		Server server = new Server(args[0], Integer.parseInt(args[1]), 20);
-		server.start();
-	}
-	
 	private int port; 
     private String host; 
     private int backlog;
     private ServerSocket serverSocket; 
-    private ExecutorService threadPool; 
+    private ExecutorService clientThreads; 
+    private Map<String, String> users;
 
     public Server(String host, int port, int backlog) {
         this.port = port;
         this.host = host;
         this.backlog = backlog;
-        try {
-			this.serverSocket = new ServerSocket(this.port, this.backlog, (InetAddress.getByName(this.host)));
-		} catch (IOException e) {
-			System.out.println("Server was unable to start - ServerSocket could not be established using port: "
-		+ port + " at host: " + host);
-			e.printStackTrace();
-		}
-        this.threadPool = Executors.newCachedThreadPool();
+        this.clientThreads = Executors.newCachedThreadPool();
+        this.users = new HashMap<String, String>();
     }
     
-    
-    public void start() {
+    public void start() throws UnknownHostException, IOException {
+    		this.serverSocket = new ServerSocket(this.port, this.backlog, (InetAddress.getByName(this.host)));
+        	System.out.println(this.serverSocket);
         while(true){
         		try {
-        			//open new socket at server for requesting client
 	        		Socket socketForClient = this.serverSocket.accept();
-	        		BufferedReader threadInput = new BufferedReader(new InputStreamReader(socketForClient.getInputStream()));
-	    			DataOutputStream threadOutput = new DataOutputStream(socketForClient.getOutputStream());
-	    			ChatThread thread = new ChatThread(socketForClient, threadInput, threadOutput);
-	        		threadPool.execute(thread);
+	        		System.out.println(socketForClient);
+	        		BufferedReader fromClient = new BufferedReader(new InputStreamReader(socketForClient.getInputStream()));
+	    			DataOutputStream toClient = new DataOutputStream(socketForClient.getOutputStream());
+	        		this.clientThreads.execute(new ChatThread(this, socketForClient, fromClient, toClient));
         		} catch (IOException e) {
-        			//continue looping in search for new client connections
+        			System.out.println("Socket could not be created!");
         		}
         }
     }
+    
+    public void addUser(String user, String pw) {
+		users.put(user, pw);
+    }
+    
+    public boolean checkUser(String user) {
+    		if (users.containsKey(user)) {
+    			return true;
+    		} else {
+    			return false;
+    		}
+    }
+   
+    public static void main (String [] args)  {
+		Server server = new Server(args[0], Integer.parseInt(args[1]), 20);
+		try {
+			server.start();
+		} catch (IOException e) {
+			System.out.println("Server could not be started with provided args! Please ensure args are correct.");
+		}
+	}
     
 }
