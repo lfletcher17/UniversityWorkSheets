d1d6c66a6e93067f2c1c8fa48d47c7594b93e90a
lxf736 <lxf736@cs.bham.ac.uk>
2018-08-03 15:00:27 +0100

    spotify project

---
+++ EventSuggester/app/src/main/java/com/example/android/eventsuggester/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.eventsuggester;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.spotify.sdk.android.player.Player;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
